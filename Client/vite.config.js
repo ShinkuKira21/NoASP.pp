e3c4d1171d7c1705d3a1e8119d8019798f2afcd9
Edward Patch <edwardpatch1@gmail.com>
2022-05-23 01:33:07 +0100

    Enabled HTTPS

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
     server: {
         https: true,
         host: '0.0.0.0',
         port: 1500
-      },
-    plugins: [ mkcert() ]
+      }
 });
